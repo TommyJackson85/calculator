cec928d79034cb1b0cee3d85f33d03d1dde01703
ThomasJ85 <tom.jackson50@gmail.com>
2018-05-18 22:42:07 +1000

    multiple changes (.ie permissions, arrays, more use of functions.)
    Removed the newAnswer array. I am now using the newNumber array to
    store the answer because. Made several changes to my reusable
    functions.
    Removed my boolean variables (except the allowDec var).
    
    Fixed issues relating to pushing of the decimal into the calculation
    array.

--- js/main.js
+++ js/main.js
@@ -1,125 +1,127 @@
-//calculation screen
+//calculation and input screen
 const displayedCalc = document.getElementById("displayedCalc");
 const displayedInput = document.getElementById("displayedInput");
 
-//true and values to allow and prevent functionalities
-let disableDec = false; let allowNumber = true; let allowOperator = false; let calculationFinished = false;
-// calculation array and input arrays for used as base for calculations. 
-const calculation = []; const newNumber = []; const newOperator = []; const newAnswer = [];
+let disableDec = false; //decimal button is disabled
 
-//Array reset funtions (except displayes)
-const empty = function(array){ array.splice(0, array.length);} //oneArray
-const clearAll = function() { //allArrays
-	empty(calculation); empty(newOperator); empty(newNumber); empty(newAnswer);
-	disableDec = false; allowNumber = true; allowOperator = false; calculationFinished = false;
+// calculation array and input arrays for used as base for calculations. 
+const calculation = []; const newNumber = []; const newOperator = [];
+
+//Array reset funtions
+const empty = function(array){ 
+	array.splice(0, array.length);
+} //oneArray
+const clearInputs = function(){
+	empty(newOperator); empty(newNumber); //clears input arrays
 }
-const clearedDisplay = function(){
+const clearAll = function() {
+	empty(calculation); clearInputs(); //clears all arrays
+	disableDec = false;	
+}
+const clearAllAndDisplay = function(){
+	clearAll();
 	displayedCalc.innerHTML = "cleared";
-	displayedInput.innerHTML = "0";
+	displayedInput.innerHTML = "0";//tells user everything is cleared
 }
-//moves from input array data to calculation array
+//moves data from input arrays to calculation array
 const pushToCalculation = function(input){
-	calculation.push(input[0]);
-	input.splice(0,input.length);
+	if (input.constructor === Array) {
+		calculation.push(input[0]);//should be an opperator
+		empty(input);
+	} else {
+		if([".", "-."].includes(input)){
+			input += "0";
+		}
+		if(input.length > 0){
+			calculation.push(input);
+		};//should be a number
+	}
 }
-//number and decimal buttons
+const displayCalculation = function(){
+	if(calculation.join(" ").length > 45){
+		//keeps it within the right size for the screen
+		let lengthToCut = calculation.join(" ").length - 48; 
+		displayedCalc.innerHTML = "..." + calculation.join(" ").slice(lengthToCut);
+	} else if (calculation.length <= 45) {
+		if (calculation.length == 0){
+			displayedCalc.innerHTML = "cleared";
+		} else {
+			displayedCalc.innerHTML = calculation.join(" ");
+		}
+	}
+}
+//number buttons and decimal button
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "Dec"];//used for id reference
 for (var key of numbers){
 	let number = document.getElementById("num" + key);
 	number.onclick = function(){
-		//functional permisions changes
-		if(allowNumber == true){
-			if(calculationFinished == true){
+		//criteria to be used 
+		if(calculation.length == 0 || newOperator.length == 1 
+		|| ['=', '+', '-', 'x', '/'].includes(calculation[calculation.length-1])){
+			if(calculation[calculation.length-1] == "="){ 
 				clearAll();
 			}
-		}
-		if(newOperator.length == 1 && newNumber.length >= 0) { 
-			pushToCalculation(newOperator);
-		}
-		
-		//pushes decimal OR number, building a number
-	   	if (this.value == "."){
-			if(disableDec == false) { //alows decimal once
+			if(newOperator.length == 1 && newNumber.length >= 0) { 
+				pushToCalculation(newOperator);
+			}
+			//pushes decimal OR number, building a number
+		   	if (this.value == "."){
+				if(disableDec == false) { //alows decimal once
+					newNumber.push(this.value);
+					disableDec = true;
+				} 
+			} else {
 				newNumber.push(this.value);
-				disableDec = true;
-			} 
-		} else {
-			newNumber.push(this.value);
+			}
+			displayCalculation();
+			displayedInput.innerHTML = newNumber.join("");
 		}
-
-		//displaying data
-		if (calculation.length == 0) { 
-			displayedCalc.innerHTML = "cleared"; 
-		} else {
-			displayedCalc.innerHTML = calculation.join(" ");
-		}
-		displayedInput.innerHTML = newNumber.join("");
-		allowOperator = true;
 	}
 }
-
 //operator buttons
 const operatorIds = ["add", "subtract", "multiply", "divide", "answer"];
 for (var key of operatorIds) {
 	let operator = document.getElementById(key);
-	operator.onclick = function() {
-		if(allowOperator == true){
-			//changes functional permissions
-			allowNumber = true; disableDec = false;	
-
+	operator.onclick = function(){
+		//if(calculation.length >= 1 && (newNumber.length >= 1 || newOperator.length >= 1 || displayedInput.innerHTML = "removed")){
+		//changes functional permissions
+		if(calculation.length >= 0){
+			disableDec = false;
 			if (this.value === "="){
 				//FINISHES calculation
-				if (calculationFinished == false){
+				if (calculation[calculation.length-1] != "="){
 					//adds last number
-					calculation.push(newNumber.join(""));
+					pushToCalculation(newNumber.join(""));
 					empty(newNumber);
-
 					//changes string to real calculation
-					let realCalculation = calculation.join(" ").replace("x", "*");
-					newAnswer.push(eval(realCalculation + ""));//calulates, then turns back to string for manipulation
-					displayedCalc.innerHTML = calculation.join(" ") + " =";//adding " =" for display
+					let realCalculation = calculation.join(" ").replace(/x/g , "*");
+					newNumber.push(eval(realCalculation) + "");//calulates, then turns back to string for manipulation
+					calculation.push("=");//only for display
+					displayCalculation();
 					displayedInput.innerHTML = eval(realCalculation); //displays answer
-					calculationFinished = true;
 				}
-
-			} else { //other operators
-				
-				if (newAnswer.length == 1){
-					//If Calculation is finished and dispalayed, a new calculation starts with the previous Answer as the first number
-					empty(calculation); pushToCalculation(newAnswer);
-				} else {
-					//If Calculation is NOT finished, pushed number previously created
-					calculation.push(newNumber.join("")); empty(newNumber);
-				}
-				
-				//replaces previous operator (would have been pushed into calculation IF new number was created afterwards)
-				empty(newOperator)
+			} else { //other operators				
+				if(calculation[calculation.length-1] == "="){//indicates calculation is finished
+					empty(calculation);
+				}		
+				pushToCalculation(newNumber.join(""));//either new number or previous answer
+				empty(newNumber);
+				empty(newOperator);
 				newOperator.push(this.value);
-				
-				displayedCalc.innerHTML = calculation.join(" ");
+				displayCalculation();
 				displayedInput.innerHTML = this.value;
-				calculationFinished = false;
 			}
-			//restarts a new number
-			empty(newNumber);
 		}
 	}
 }
 
 const negative = document.getElementById("negative");
 negative.onclick = function(){
-	if(calculationFinished == true){
-		if(newAnswer[0].charAt(0) === "-"){
-			newAnswer[0] = newAnswer[0].slice(1);
+	if(newNumber[0].length >= 1){
+		if(newNumber[0].charAt(0) === "-"){
+			newNumber[0] = newNumber[0].slice(1);
 		} else {
-			newAnswer[0] = "-" + newAnswer[0];
-		}
-		displayedInput.innerHTML = newAnswer.join("");
-	} else {
-		if(newNumber[0] == "-"){
-			newNumber.splice(0,1);//pop to front
-		} else {
-			newNumber.unshift("-");
+			newNumber[0] = "-" + newNumber[0];
 		}
 		displayedInput.innerHTML = newNumber.join("");
 	}
@@ -127,43 +129,29 @@ negative.onclick = function(){
 
 const clear = document.getElementById("clear");
 clear.onclick = function() {
-	clearAll(); clearedDisplay();
+	clearAllAndDisplay();
 }
+
 //changes input number (new number or answer) to a negative or reverts back to positive
 const remove = document.getElementById("remove");
 remove.onclick = function(){
-	const undoSetting = function(){
-		//corrects display and permissions
-		displayedCalc.innerHTML = calculation.join(" ");
-		displayedInput.innerHTML = "removed";
-		disableDec = true;	allowNumber = false;  allowOperator = true;
-	}
-	if(calculationFinished == true){
-		empty(newAnswer); undoSetting();
-		calculationFinished = false;
-	} else {
-		if (calculation.length > 1){
-			if (newOperator == 1 && newNumber == 0){
-				empty(newOperator);
-			} else if (newOperator == 0 && newNumber >= 1){
-				empty(newNumber);
-				calculation.splice(-1,1);	
-			} else {
-				calculation.splice(-2,2);
-			}
-			undoSetting();
-		} else {
-			clearAll(); clearedDisplay();
+	console.log(calculation);
+	if(newOperator.length > 0 || newNumber.length > 0){
+		if(['=', '+', '-', 'x', '/'].includes(calculation[calculation.length-1])){
+			calculation.splice(-1,1);
 		}
+		clearInputs();
+	} else {
+		calculation.splice(-2,2);
 	}
+		displayCalculation();
+		displayedInput.innerHTML = "removed";
 }
 
 const percentage = document.getElementById("percentage");
 percentage.onclick = function(){
+	disableDec == false;
 	displayedCalc.innerHTML = "percentage"
-	if(calculationFinished == true){
-		displayedInput.innerHTML = eval(newAnswer[0]) / 100;
-	} 
 	if (newNumber.length >= 1){
 		displayedInput.innerHTML = eval(newNumber.join("")) / 100;
 	}
